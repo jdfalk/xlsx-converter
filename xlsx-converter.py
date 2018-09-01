35a05de1845366e098445248df4cfc7fb7377723
Dane Lembrick <dlembrick@gmail.com>
2018-09-01 13:17:15 -0500

    First read/write.

--- xlsx-converter.py
+++ xlsx-converter.py
@@ -1,2 +1,10 @@
 # XLSX Converter
 import pandas as PD
+
+# read excel file into a dataframe, declare details about the dataframe.  We assumed all default values.
+data_xls = PD.read_excel("c:\\temp\\Book1.xlsx")
+data_xls.to_csv(
+    "c:\\temp\\Book1.csv", 
+    mode='a', 
+    index=False
+    )
