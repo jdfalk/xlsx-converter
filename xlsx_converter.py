b7282237bbaee3895f8c4e9d2838fb8e8892bc10
Johnathan Falk <johnathan.falk@gmail.com>
2018-09-01 17:28:49 -0400

    Add try except.

--- xlsx_converter.py
+++ xlsx_converter.py
@@ -60,11 +60,22 @@ def main():
                 logging.debug("full_path is: %s", full_path)
                 # read excel file into a dataframe, declare details about the dataframe.
                 # We assumed all default values.
-                data_xls = PD.read_excel(
-                    io=full_path,
-                    sheet_name=int(args.sheet),
-                    # skiprows=0
-                    )
+                try:
+                    data_xls = PD.read_excel(
+                        io=full_path,
+                        sheet_name=int(args.sheet),
+                        skiprows=0
+                        )
+                except IndexError as err:
+                    logging.error("Error occured: %s", err)
+                    logging.info("Continuing using sheet 0 for file %s", full_path)
+                    data_xls = PD.read_excel(
+                        io=full_path,
+                        sheet_name=0,
+                        skiprows=0
+                        )
+                    continue
+
                 data_xls.to_csv(
                     output_file,
                     mode='a',
